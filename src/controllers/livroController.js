f937a408b5480ec88696d55049994a19e09d4bb9
dinah-parag <dinah.dantass@gmail.com>
2026-03-10 21:59:13 -0300

    Feat: Conexão API, Busca, Categorias

---
+++ src/controllers/livroController.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { identificarCategoria } from '../utils/mapeadorDewey.js';
+
+export const buscarLivroExterno = async (req, res) => {
+    const { titulo } = req.query; // Pega o título enviado na URL
+
+    try {
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${titulo}`);
+        const dados = response.data.items[0].volumeInfo; // Pega o primeiro resultado
+
+        // Descobrindo o CCD e a Cor
+        const infoDewey = identificarCategoria(dados.categories ? dados.categories[0] : "");
+
+        const novoLivro = {
+            titulo: dados.title,
+            autor: dados.authors ? dados.authors[0] : "Autor Desconhecido",
+            ano: dados.publishedDate ? dados.publishedDate.split('-')[0] : "N/A",
+            cdd: infoDewey.codigo,
+            categoriaNome: infoDewey.nome,
+            corSugerida: infoDewey.cor, 
+            resumo: dados.description || "Sem resumo disponível.",
+            status: "Não lido",
+            nota: 0
+        };
+
+        res.status(200).json(novoLivro);
+    } catch (error) {
+        res.status(500).json({ mensagem: "Erro ao buscar livro", erro: error.message });
+    }
+};
