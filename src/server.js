2c54e1ca9b6b9864c18176227f466962dea465e3
dinah-parag <dinah.dantass@gmail.com>
2026-03-16 22:04:53 -0300

    Feat: MongoDP conection

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import routes from './routes/livroRoutes.js';
 
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGODB_URI;
 
 app.use(express.json());
-
-// Usar as rotas definidas em livroRoutes.js
 app.use(routes);
 
+async function conectarBanco() {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("✅ Conectado ao MongoDB com sucesso!");
+    } catch (erro) {
+        console.error("❌ Erro ao conectar ao MongoDB:", erro.message);
+    }
+}
+
+conectarBanco();
+
 app.get('/', (req, res) => {
-    res.status(200).send('API da Biblioteca Dewey Rodando e pronta para buscas!');
+    res.status(200).send('API da Biblioteca Dewey com MongoDB Conectado!');
 });
 
 app.listen(PORT, () => {
